5833f0c46edb86b7f79b343fe5b30d1810166d47
AIe !! <makramjandar@gmail.com>
2020-02-21 07:58:49 +0100

    Update progressingEcho.sh

--- progressingEcho.sh
+++ progressingEcho.sh
@@ -7,10 +7,24 @@
 #  vargreen=$(egre "Grass is green")
 #  echo "Coming next: $vargreen"
 
-ct() { echo -e " \e[0;$2m$1\e[0m" }
-cg() { echo $(ct "$1" "32") | pv -qL 10 }
-cr() { echo $(ct "$1" "31") | pv -qL 10 }
-cb() { echo $(ct "$1" "34") | pv -qL 10 }
-cp() { echo $(ct "$1" "35") | pv -qL 10 }
-cy() { echo $(ct "$1" "33") | pv -qL 10 }
-cc() { echo $(ct "$1" "36") | pv -qL 10 }
+function ct() { 
+  echo -e " \e[0;$2m$1\e[0m" 
+  }
+function cg() { 
+  echo $(ct "$1" "32") | pv -qL 10 
+  }
+function cr() { 
+  echo $(ct "$1" "31") | pv -qL 10 
+  }
+function cb() { 
+  echo $(ct "$1" "34") | pv -qL 10 
+  }
+function cp() { 
+  echo $(ct "$1" "35") | pv -qL 10 
+  }
+function cy() { 
+  echo $(ct "$1" "33") | pv -qL 10 
+  }
+function cc() { 
+  echo $(ct "$1" "36") | pv -qL 10 
+  }
