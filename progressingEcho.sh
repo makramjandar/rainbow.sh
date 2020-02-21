6e7c18a7091da5d379fea96c115b40bcd7666244
AIe !! <makramjandar@gmail.com>
2020-02-21 02:02:44 +0100

    Update and rename rainbow.sh to progressingEcho.sh

--- progressingEcho.sh
+++ progressingEcho.sh
@@ -8,9 +8,9 @@
 #  echo "Coming next: $vargreen"
 
 ct() { echo -e " \e[0;$2m$1\e[0m" }
-cg() { echo $(ct "$1" "32") }
-cr() { echo $(ct "$1" "31") }
-cb() { echo $(ct "$1" "34") }
-cp() { echo $(ct "$1" "35") }
-cy() { echo $(ct "$1" "33") }
-cc() { echo $(ct "$1" "36") }
+cg() { echo $(ct "$1" "32") | pv -qL 10 }
+cr() { echo $(ct "$1" "31") | pv -qL 10 }
+cb() { echo $(ct "$1" "34") | pv -qL 10 }
+cp() { echo $(ct "$1" "35") | pv -qL 10 }
+cy() { echo $(ct "$1" "33") | pv -qL 10 }
+cc() { echo $(ct "$1" "36") | pv -qL 10 }
