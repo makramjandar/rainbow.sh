98ab42c24edc76cb2e272668c7ecc1c7040128f9
AIe !! <makramjandar@gmail.com>
2020-02-21 01:32:04 +0100

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -4,45 +4,16 @@
 #  Bash helper functions to put colors on your scripts
 #
 #  Usage example:
-#  vargreen=$(echogreen "Grass is green")
+#  vargreen=$(egre "Grass is green")
 #  echo "Coming next: $vargreen"
-#
 
 __RAINBOWPALETTE="0"
 
-function __colortext()
-{
-  echo -e " \e[$__RAINBOWPALETTE;$2m$1\e[0m"
-}
-
- 
-function echogreen() 
-{
-  echo $(__colortext "$1" "32")
-}
-
-function echored() 
-{
-  echo $(__colortext "$1" "31")
-}
-
-function echoblue() 
-{
-  echo $(__colortext "$1" "34")
-}
-
-function echopurple() 
-{
-  echo $(__colortext "$1" "35")
-}
-
-function echoyellow() 
-{
-  echo $(__colortext "$1" "33")
-}
-
-function echocyan() 
-{
-  echo $(__colortext "$1" "36")
-}
+function __colortext() { echo -e " \e[$__RAINBOWPALETTE;$2m$1\e[0m" }
+cg() { echo $(__colortext "$1" "32") }
+cr() { echo $(__colortext "$1" "31") }
+cb() { echo $(__colortext "$1" "34") }
+cp() { echo $(__colortext "$1" "35") }
+cy() { echo $(__colortext "$1" "33") }
+cc() { echo $(__colortext "$1" "36") }
 
