e5ce68d215b1a94f33b45e47e8c49a957d4a0cf5
AIe !! <makramjandar@gmail.com>
2020-02-21 01:35:13 +0100

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -7,13 +7,10 @@
 #  vargreen=$(egre "Grass is green")
 #  echo "Coming next: $vargreen"
 
-__RAINBOWPALETTE="0"
-
-function __colortext() { echo -e " \e[$__RAINBOWPALETTE;$2m$1\e[0m" }
-cg() { echo $(__colortext "$1" "32") }
-cr() { echo $(__colortext "$1" "31") }
-cb() { echo $(__colortext "$1" "34") }
-cp() { echo $(__colortext "$1" "35") }
-cy() { echo $(__colortext "$1" "33") }
-cc() { echo $(__colortext "$1" "36") }
-
+ct() { echo -e " \e[0;$2m$1\e[0m" }
+cg() { echo $(ct "$1" "32") }
+cr() { echo $(ct "$1" "31") }
+cb() { echo $(ct "$1" "34") }
+cp() { echo $(ct "$1" "35") }
+cy() { echo $(ct "$1" "33") }
+cc() { echo $(ct "$1" "36") }
